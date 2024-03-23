c6ee2327bed6957e257435150bc5fc8637c355dd
Andres Cardenas <a.cardenasj@uniandes.edu.co>
2024-03-23 11:52:19 -0500

    First commit

---
+++ app/src/test/java/com/miso202402/sa_front_movil/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.miso202402.sa_front_movil
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
